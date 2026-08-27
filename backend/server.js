fb6900cc2b8f1f2e0c91dfc5c0af39521b900737
khennyboy <abdullateefkehinde848@gmail.com>
2026-08-27 23:02:07 +0100

    third  commit

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import { router as productRoutes } from "./routes/products.route.js";
-import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
 const __dirname = path.resolve();
-app.use(express.json()); // allow us to accept JSON data in the body
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("/*splat", (_, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
-app.use("/api/products", productRoutes);
-
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http:localhost:8000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
